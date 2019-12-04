1948fef2e6892eee260890440973a2093034c068
kishorethecoder <kishorevalwin@hotmail.com>
2019-12-04 13:55:35 +0530

    Revert "fix: side-effect by #502"
    This reverts commit 73eb42df36857aa440bedae32d7335a10e011788.

--- src/js/convertor.js
+++ src/js/convertor.js
@@ -105,8 +105,7 @@ class Convertor {
    * @returns {string} html text
    */
   _markdownToHtml(markdown, env) {
-    // should insert data-tomark-pass in the opening tag
-    markdown = markdown.replace(/<(?!\/)([^>]+)([/]?)>/g, '<$1 data-tomark-pass $2>');
+    markdown = markdown.replace(/<([^>]+)([/]?)>/g, '<$1 data-tomark-pass $2>');
     // eslint-disable-next-line
         const onerrorStripeRegex = /(<img[^>]*)(onerror\s*=\s*[\"']?[^\"']*[\"']?)(.*)/i;
     while (onerrorStripeRegex.exec(markdown)) {
